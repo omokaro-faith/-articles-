acb46b8a8401909227dc3bb2a799de797debf158
omokaro-faith <omokaro.faith@andela.com>
2020-05-03 19:05:00 +0100

    update project config settings

--- webpack.config.js
+++ webpack.config.js
@@ -6,48 +6,74 @@ const dotenv = require('dotenv');
 const env = dotenv.config().parsed;
 
 const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
+	prev[`process.env.${next}`] = JSON.stringify(env[next]);
+	return prev;
 }, {});
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  node: {
-    fs: "empty"
- },
- plugins: [
-  new webpack.DefinePlugin(envKeys),
-],
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.scss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-    ],
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'src'),
-    compress: true,
-    port: 9000,
-    host: '0.0.0.0',
-    port: 8080,
-  }
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	node: {
+		fs: 'empty',
+	},
+	plugins: [new webpack.DefinePlugin(envKeys)],
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+			{
+				test: /\.scss$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.less$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					{
+						loader: 'less-loader', // compiles Less to CSS
+						options: {
+							javascriptEnabled: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.svg$/,
+				use: [
+					{
+						loader: 'svg-url-loader',
+						options: {
+							limit: 10000,
+						},
+					},
+				],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['*', '.mjs', '.js', '.jsx', '.less'],
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'src'),
+		compress: true,
+		port: 9000,
+		host: '0.0.0.0',
+		port: 8080,
+	},
 };
